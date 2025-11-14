18199bc495c86445aac00a7b14eaaaaa40a5aad7
Johan PIQUET <johan.piquet@gmail.com>
2025-11-14 17:45:23 +0100

    Correction for scripts

--- src/@tool-jopinb/index.ts
+++ src/@tool-jopinb/index.ts
@@ -1,16 +1,11 @@
-import {createRequire} from "node:module";
+import {jopiLauncherTool} from "jopi-rewrite/loader-tools";
 
 const VERSION = "2.0.0";
 
 export function useEngine(engine: string) {
-    const myRequire = createRequire(process.cwd());
-
     if (process.env.JOPI_LOG==="1") {
-        let resolvedDir = myRequire.resolve("jopi-rewrite/loader-tools");
-        console.log("JopiN - jopi-rewrite/loader-tools found at " + resolvedDir);
         console.log("JopiN - Loader Version" + VERSION + " - engine=" + engine);
     }
 
-    const lib = myRequire("jopi-rewrite/loader-tools");
-    lib.jopiLauncherTool(engine).then();
+    jopiLauncherTool(engine).then();
 }
